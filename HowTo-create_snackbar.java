4938e65166b92d79e2837dc780fb2f71ac52b5e3
LucaSalmi <luca.salmi90@gmail.com>
2021-10-01 11:06:21 +0200

    1 how to and 1 test file

---
+++ HowTo-create_snackbar.java
@@ -0,0 +1,26 @@
+import android.app.Activity;
+import android.view.View;
+
+import com.example.bmicalculator.MainActivity;
+import com.google.android.material.snackbar.Snackbar;
+
+/*
+class Scratch {
+
+    Activity mainActivity = MainActivity.this;
+
+    public static void main(String[] args) {
+
+        showSnackbar(mainActivity);
+        
+    }
+
+    public void showSnackbar(Activity activity){
+        View rootView = activity.getWindow().getDecorView().findViewById(R.id.main_layout);
+        Snackbar.make(rootView,R.string.floating_message,Snackbar.LENGTH_SHORT).show();
+    }
+
+}
+
+
+ */
---
+++ cast to double test'.java
@@ -0,0 +1,11 @@
+class Scratch {
+    public static void main(String[] args) {
+        double a = 175.0;
+        int b = 175;
+        String c = "175";
+        //System.out.println(Double.parseDouble(c));
+        //System.out.println((double) b);
+        //System.out.println(Integer.parseInt(c));
+        System.out.println(a/100);
+    }
+}
